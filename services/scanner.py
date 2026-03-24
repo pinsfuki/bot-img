8b0a6527d41a079bd13ab05ce804150a61dcdb67
pins <baris.gabin@gmail.com>
2026-03-24 09:21:58 +0100

    scanner ajout de la detection des images mais pas encore op

--- services/scanner.py
+++ services/scanner.py
@@ -3,12 +3,15 @@ import discord
 async def scanner(channel, date_debut, date_fin):
 
     salon = channel.history()
-    async for x_date in salon:
-        if x_date.created_at > date_fin:
+    image = 0
+    async for x_message in salon:
+        if x_message.created_at > date_fin:
             continue
 
-        elif date_debut <= x_date.created_at <= date_fin:
-            print('ok')
+        elif date_debut <= x_message.created_at <= date_fin:
+                if x_message.attachments > 0:
+                    image+=1
+                print(image)
 
         else:
             break
