87e43751fe15adb75651cdfa0436e564ad8eaa96
pins <baris.gabin@gmail.com>
2026-03-30 23:25:58 +0200

    affichage total images sur discord dans le tchat

--- commands/export_photos.py
+++ commands/export_photos.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import asyncio
 from services.date_parser import parse_date
 from services.scanner import scanner
 import discord
@@ -6,16 +7,25 @@ import discord
 
 def setup(bot):
     @bot.slash_command(name='export_photos', description='Exporter les photos choisi par date et par salon')
-    async def export_photos(ctx, date_debut : str, date_fin  : str , nom_export  : str):
+    async def export_photos(ctx, date_debut : str, date_fin  : str , nom_export  : str ):
         salon = ctx.channel
         date_debut_converti = parse_date(date_debut)
         date_fin_converti = parse_date(date_fin, fin=True)
         embed = discord.Embed(
             title = 'Info',
-            description = (f'Ok je vais chercher les photos de {date_debut} à {date_fin} et ton fichier se nommera {nom_export} :)'),
+            description = (f'Ok je vais chercher les photos de {date_debut} à {date_fin} et ton fichier se nommera {nom_export} :smile: '),
             color=discord.Color.orange()
         )
 
+
+
         await ctx.respond(embed=embed)
+
         total_images = await scanner(salon,date_debut_converti,date_fin_converti)
-        print(total_images)
+        reponse = discord.Embed(
+            title = 'Trouvailles',
+            description = (f"J'ai trouvé un total de {total_images} image(s)  :smile: "),
+            color=discord.Color.orange()
+        )
+        await asyncio.sleep(3)
+        await ctx.followup.send(embed=reponse)
